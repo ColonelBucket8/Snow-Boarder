1681207a8356be6774f826ce8d89d4f7a04e6d79
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-02 14:03:42 +0800

    Add crash detector script to player to detect if player's head hit the ground

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        // if (other.gameObject.CompareTag("Finish")) return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("You hit your head!");
+
+        }
     }
 }
