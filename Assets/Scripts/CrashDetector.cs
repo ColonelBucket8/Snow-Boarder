94ffda2fd26f0ddfa1d0954d3f72316cd2df5124
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-02 20:53:48 +0800

    Play particle system when crashing or finish

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,20 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] private float loadDelay = 0.5f;
+    [SerializeField] private ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem.Stop();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // if (other.gameObject.CompareTag("Finish")) return;
 
         if (other.gameObject.CompareTag("Ground"))
         {
+            particleSystem.Play();
             Invoke(nameof(ResetLevel), loadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,13 +3,19 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem particleSystem;
 
 
+    void Start()
+    {
+        particleSystem.Stop();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Invoke(nameof(ReloadScene), loadDelay);
+            particleSystem.Play();
         }
     }
 
