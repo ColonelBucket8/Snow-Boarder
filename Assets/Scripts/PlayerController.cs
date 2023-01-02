c7f0720a7533caa7bd75e60f4e2a5b91154517f4
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-02 21:13:04 +0800

    Add keybinding to boost speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 3f;
+    [SerializeField] private float boostSpeed = 30f;
+    [SerializeField] private float baseSpeed = 20f;
 
     private Rigidbody2D rb2d;
+    private SurfaceEffector2D surfaceEffector2D;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -24,5 +46,10 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(-torqueAmount);
         }
+    }
+
+    void OnTriggerEnter2D()
+    {
+
     }
 }
